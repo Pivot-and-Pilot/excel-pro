610fdd94cd0b86c96716ba792700158768f54cc2
TuanPham303 <phamcongtuan303@gmail.com>
2018-03-08 12:18:28 -0800

    add download all files and answer keys

--- js/shop.js
+++ js/shop.js
@@ -12,6 +12,7 @@ jQuery(document).ready(function($){
                     $('#video-1')[0].play();
                     $('#video-2')[0].pause();
                     $('#video-3')[0].pause();
+                    $('.picture-laptop-alternate > .figure-price').css('right', 'unset');
                 }
                 if ($(this).attr('data-iframe-ref') === 'video_two'){
                     $('#video-1').css('opacity', '0');
@@ -21,6 +22,7 @@ jQuery(document).ready(function($){
                     $('#video-2')[0].play();
                     $('#video-1')[0].pause();
                     $('#video-3')[0].pause();
+                    $('.picture-laptop-alternate > .figure-price').css('right', 'unset');
                 }
                 if ($(this).attr('data-iframe-ref') === 'video_three'){
                     $('#video-1').css('opacity', '0');
@@ -30,6 +32,7 @@ jQuery(document).ready(function($){
                     $('#video-2')[0].pause();
                     $('#video-1')[0].pause();
                     $('#video-3')[0].play();
+                    $('.picture-laptop-alternate > .figure-price').css('right', '160px');
                 }
             })
         }
