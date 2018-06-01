008dd4cdd486218c89d8b0ef44c51e94bd71bf04
TuanPham303 <phamcongtuan303@gmail.com>
2018-06-01 12:10:42 -0700

    fix countries not showing up

--- js/checkout.js
+++ js/checkout.js
@@ -1,11 +1,6 @@
 jQuery(document).ready(function($) {
   (function addHiddenCountryAndState() {
-    $("#select2-billing_country-container").on("click", function() {
-      $(".select2-results__option").each(function() {
-        $(this).attr("aria-selected", "false");
-      });
-    });
-    $(".select2-selection__arrow").on("click", function() {
+    $(".woocommerce-input-wrapper").on("click", function() {
       $(".select2-results__option").each(function() {
         $(this).attr("aria-selected", "false");
       });
